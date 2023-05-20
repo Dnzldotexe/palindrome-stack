88d68d56fb994e74c20019f684b0cad964096383
Dnzl.exe <104302220+Dnzldotexe@users.noreply.github.com>
2023-05-20 09:30:34 +0000

    cleaned and improved code logic

--- palindrome.py
+++ palindrome.py
@@ -2,43 +2,48 @@
     # Test case:
     # A man, a plan, a canal, Panama.
 
-from SimpleStack import Stack                                                   # Imports the necessary python module
+from SimpleStack import Stack                               # Imports the necessary python module
 
 # Time Complexity: O()
 # Space Complexity: O()
-def is_palindrome():
+def is_palindrome(word):
 
-    alphabet_lower = "abcdefghijklmnopqrstuvwxyz"                               # Creates a string of lowercase letters
+    lowercase = "abcdefghijklmnopqrstuvwxyz"                # Creates a string of lowercase letters
 
-    word = input("\nPlease Enter a String: ").strip()                           # Asks the user for input
+    original_stack = Stack(len(word))                       # Creates two ojects of the Stack
+    copy_stack = Stack(len(word))                           # based on length of the word
 
-    while not word:                                                             # Checks if string is empty
-        print("String is Empty")
-        word = input("\nPlease Enter a String: ").strip()
-    
-    original_stack = Stack(len(word))                                           # Creates two ojects of the Stack
-    copy_stack = Stack(len(word))                                               # based on length
-
-    for letter in word:                                                         # Loops over each letters in word
-        if not original_stack.isFull() and letter.lower() in alphabet_lower:    # Push letters on both stacks if not full
-            original_stack.push(letter.lower())                                 # and if letters are in alphabet_lower
+    for letter in word:                                     # Loops over each letters in word
+        if letter.lower() in lowercase:                     # if the letter is in lowercase
+            original_stack.push(letter.lower())
             copy_stack.push(letter.lower())
-    
-    if original_stack.isEmpty():                                                # returns if the stack has no letters
+
+    if original_stack.isEmpty():                            # Returns if the stack has no letters
         print("No letters were found.")
-        return
+        return False
 
-    original_word = ''                                                          # Creates an empty string
-    reverse = ''                                                                # Build the reversed version
-    while not original_stack.isEmpty():                                         # by popping the original_stack until empty
+    original_word, reverse = '',''                          # Build the orginal and reversed version
+    while not original_stack.isEmpty():                     # by popping the original_stack until empty
         reverse += original_stack.pop()
-        original_word = copy_stack.pop() + original_word                        # Adding each character after the other
+        original_word = copy_stack.pop() + original_word    # Adding each character after the other
 
-    if original_word != reverse:                                                # Checks if both string are not the same
-        print(f"'{word}' is not a Palindrome")                                  # prints not a palindrome if not
+    if original_word != reverse:                            # Returns False if not palindrome
+        return False
 
-    else:                                                                       # prints palindrome if both are identical
-        print(f"'{word}' is a Palindrome")                                      
+    return True                                             # Returns True if palindrome
 
 
-is_palindrome()                                                                 # runs the function
+def main():
+
+    word = input("\nPlease Enter a String: ").strip()       # Asks the user for input
+
+    while not word:                                         # Loops until string is not empty
+        print("String is Empty.")
+        word = input("\nPlease Enter a String: ").strip()
+
+    result = is_palindrome(word)                            # Stores the result of the function call
+
+    print(f"Result: {result}.")                             # Prints the result
+
+
+main()                                                      # Runs the function
